10f3172db88effcc5aae43d2c59adc81e2d200e7
Hà Hoàng Hảo <112957852+HoangHao1009@users.noreply.github.com>
2024-02-17 08:35:37 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name = 'autoanalyzer',
+  version = '0.1',
+  author = 'Ha Hoang Hao',
+  packages = ['Analyzer'],
+  description = 'automatically analyze marketing metrics',
+  install_requires = ['pandas', 'scikit-learn]
+)
